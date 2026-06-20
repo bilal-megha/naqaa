ea1fe921d8bd06331dc9f4bd61ade2fa1ddc20bb
bilal-megha <meghamel2012@gmail.com>
2026-06-20 10:10:07 -0700

    Delete src directory
    Delete src directory

--- src/store/utils.js
+++
@@ -1,8 +0,0 @@
-export function showToast(msg, isErr = false) {
-  document.querySelectorAll('.toast').forEach(t => t.remove())
-  const t = document.createElement('div')
-  t.className = 'toast' + (isErr ? ' err' : '')
-  t.textContent = msg
-  document.body.appendChild(t)
-  setTimeout(() => t.remove(), 2800)
-}
